53faaf30b428022592db3b540c211f7b7ff997d5
KalminX <noriabure@gmail.com>
2024-07-07 17:31:29 +0100

    added environment

--- user_auth_and_org/user_auth_and_org/settings.py
+++ user_auth_and_org/user_auth_and_org/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v3$jv-))th395m_%%8x3h(qmq)-c2$vb3jt@dc5%w5r03o)-k-'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
